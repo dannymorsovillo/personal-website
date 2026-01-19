01d5292cf610864e103f22be56b7931c91414a29
dannymorsovillo <dangino3130@gmail.com>
2026-01-19 05:02:14 -0600

    Initial commit - Vite and Vue setup

---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="main-container">
+    <h1> Daniel (Danny) Morsovillo </h1>
+    <h2> Welcome to my portfolio </h2>
+    <a href = "www.linkedin.com/in/danielmorsovillo" class ="nav-link">
+       
+    </a>
+
+    <a href = "www.github.com/danmorsovillo" class ="nav-link">
+       <img src = "/github.svg" alt="GitHub"/>
+    </a>
+
+ </div>
+</template>
+
+
+<script>
+
+</script>
+
+<style scoped>
+.main-container {
+    text-align: center;
+    margin-bottom: 5rem;
+}
+
+.nav-link {
+    width: 32px;
+    height: 32px;
+}
+
+</style>
